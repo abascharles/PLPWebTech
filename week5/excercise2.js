b3fa497d2245c47e2b0a37626d07bbcce1e67680
AC <abascharlesbenard@gmail.com>
2024-03-06 10:48:13 +0300

    Js variables

---
+++ week5/excercise2.js
@@ -0,0 +1,13 @@
+/*Write a program with three variables, each with the following value:
+
+The first variable contains your name
+The second variable contains your age
+The third variable contains your occupation
+Then print the variables using the console.log() method */
+
+const name = "Abas Charles";
+const age = 24;
+const occupation = "Software Engeenieering";
+
+
+console.log(name, age, occupation);
---
+++ week5/variable.js
@@ -0,0 +1,10 @@
+let message = "Hello World";
+// changing valie of the variable.
+message = "Nice weather";
+console.log(message);
+
+const max_speed = 300;
+console.log(max_speed);
+
+// var Keyword
+// used to declare variable with global scope(is acessible from anywhere)
