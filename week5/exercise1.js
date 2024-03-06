803164c00afe9e86dc122814952faeda28c426d4
AC <abascharlesbenard@gmail.com>
2024-03-06 10:16:32 +0300

    Running Javascript into the console

---
+++ week5/exercise1.js
@@ -0,0 +1,4 @@
+// Try to print your name, age, and occupation on the console.
+console.log("John Doe");
+console.log("24");
+console.log("Software Engineer");
---
+++ week5/index.js
@@ -0,0 +1,4 @@
+console.log("Hello World");
+console.log("I'm learning JavaScrip finally");
+
+// Excecution flow - node js executes things in a top down aproach.
