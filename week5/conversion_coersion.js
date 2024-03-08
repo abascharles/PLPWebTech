7b12ff452d993588b746c98c286b610b7ad73172
AC <abascharlesbenard@gmail.com>
2024-03-08 10:50:37 +0300

    Type conversion and coercion

---
+++ week5/conversion_coersion.js
@@ -0,0 +1,15 @@
+// Conversion - converting one data type into another
+/*  Thre functions are used 
+- Number()
+-String()
+-Boolean()  */
+
+let x = "7";
+let y = 5;
+
+
+console.log(Number(x)+y);  //5 is changed to string the concatenated
+
+
+// Coersion - value of on etype is implicitly converted into another type.
+// Avoid.
