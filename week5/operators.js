dcc0e148869709a731dc6f07564591ad935d5c5b
AC <abascharlesbenard@gmail.com>
2024-03-11 18:36:30 +0300

    Operators in Js

---
+++ week5/operators.js
@@ -0,0 +1,26 @@
+// Arithmetic operators
+let x = 5;
+let y = 2;
+
+x+=y; //her x now holds the value of the output
+console.log(x);
+
+console.log(10-3); 
+console.log(2 +4);
+
+// Assignment Opeartor
+let message = "Hello" + "Human"
+console.log(message);
+
+// comparison operators
+
+console.log(3===3);
+
+// logical operators 
+// logical AND - if any expression returns false, the result is false
+console.log(7>2 && 5>4);
+
+// logical OR  - if any expression returns true, the result is true
+
+
+// ! NOT operator - negates the expression, returning the opposite.
