7ba5563620fb2b03dc86d5c3fc9ff4c827297974
AC <abascharlesbenard@gmail.com>
2024-03-19 09:27:06 +0300

    Arrow function

---
+++ week5/arrowfunction.js
@@ -0,0 +1,21 @@
+// syntax 
+
+const fun = (parm1, parm2) => {
+    //Function body
+}
+
+const greetings = () => {
+    console.log('salamu');
+
+}
+
+greetings();
+
+// Single and multiline arrow functions
+const sum = (num) => num+2;
+
+console.log(sum(2));
+
+// arow fuction aalows to omit round bracket when you have exactly one parameter i.e 
+
+const plusThree = num => num + 3;
--- week5/functions2.js
+++ week5/functions2.js
@@ -19,3 +19,26 @@ function sum(a, b){
 
 let result = sum(1, 2);
 console.log(result);
+
+
+function checkAge(age){
+
+if(age > 18){
+    return 'You may get a car license';
+}
+    return 'Wait till you turn 18';
+}
+
+console.log(checkAge(20));
+console.log(checkAge(15));
+
+// variable Scope - global and local
+
+// Rest parameter - paramter that can accept any number of data as its arguments and store them as an array
+
+function printArguments(...args){
+    console.log(args);
+}
+
+printArguments(1, 2, 3, 4);
+printArguments('A', 'B', 'C');
