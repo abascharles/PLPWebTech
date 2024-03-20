8764a99d64179549f45678ed4a0e190b52a115ae
AC <abascharlesbenard@gmail.com>
2024-03-20 07:22:41 +0300

    Objects in js

---
+++ week5/objects/objects.js
@@ -0,0 +1,15 @@
+// Stores multiple values like array but stores related objects in key:value Pairs
+
+let myBook = {
+    title: 'Sebastian',
+    book: 'Kingkong',
+    describe:function(){
+        console.log(`Book title: ${this.title}`);
+
+        console.log(`Book author: ${this.author}`);
+    },
+}
+// An object item is also known as a property, with the key as property name and value as property value.
+
+
+// Acessing the value of an object use the . value or square [] bracket notation.
