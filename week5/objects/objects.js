f5cfd54380fbaeacef20e0a3587f1379a73e3ab4
AC <abascharlesbenard@gmail.com>
2024-03-20 07:41:15 +0300

    Adding elements to an objects and acessing elements of an object

--- week5/objects/objects.js
+++ week5/objects/objects.js
@@ -12,4 +12,25 @@ let myBook = {
 // An object item is also known as a property, with the key as property name and value as property value.
 
 
-// Acessing the value of an object use the . value or square [] bracket notation.
+// Acessing the value of an object use the .notation or square [] bracket notation.
+
+//.notation
+console.log(myBook.title);
+console.log(myBook.book);
+
+// [] bracket
+
+console.log(myBook['title']);
+console.log(myBook["book"]);
+
+// Adding element to an object 
+
+// add release year property
+myBook.year = 2023;
+
+// add publisher property
+myBook['publisher'] = 'Code With Nathan';
+
+console.log(myBook);
+
+// delete object  properties
